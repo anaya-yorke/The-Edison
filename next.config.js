698c9f968bf29242d7bf6bb9f3b2734eeb9ed88e
Anaya <ayorke@ggc.edu>
2025-03-04 06:55:43 -0500

    Simplify deployment configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
-    loader: 'default',
     domains: [],
-  },
-  trailingSlash: true,
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
   }
 }
 
