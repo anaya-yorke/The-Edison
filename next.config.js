83bc9fa177388f7f0503d0548e2eeb0811c0eae5
Anaya <ayorke@ggc.edu>
2025-03-04 06:12:27 -0500

    Remove GitHub Pages configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
   images: {
     loader: 'default',
     domains: [],
-  },
-  // Only use these settings for production GitHub Pages deployment
-  ...(process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true' ? {
-    assetPrefix: '/The-Edison',
-    basePath: '/The-Edison',
-  } : {})
+  }
 }
 
 module.exports = nextConfig 
